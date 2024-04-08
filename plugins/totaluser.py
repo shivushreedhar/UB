c20c3bfddfd127e34272790a35b53dd797893748
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-04-08 21:46:11 +0530

    Update totaluser.py

--- plugins/totaluser.py
+++ plugins/totaluser.py
@@ -18,6 +18,6 @@ async def start(client,message):
 	id = str(getid())
 	ids = id.split(',')
 
-	await message.reply_text(f"⚡️ All IDS : {ids}\n\n⚡️ Total User :- {total_user()}\n\n⚡️ Total Renamed File :- {total_rename}\nV Total Size Renamed :- {humanbytes(int(total_size))}",quote=True,
-                             reply_markup= InlineKeyboardMarkup([[InlineKeyboardButton("🦋 Close Menu 🦋", callback_data="cancel")]]) 
+	await message.reply_text(f" All IDS : {ids}\n\nTotal User :- {total_user()}\n\n Total Renamed File :- {total_rename}\nV Total Size Renamed :- {humanbytes(int(total_size))}",quote=True,
+                             reply_markup= InlineKeyboardMarkup([[InlineKeyboardButton(" Close Menu ", callback_data="cancel")]]) 
                              )
