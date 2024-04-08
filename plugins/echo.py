6c8f295e89e8df79b99d0f19c9225ca1acfce78d
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-04-08 22:03:03 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -57,24 +57,79 @@ LAZY_PIC = os.environ.get("LAZY_PIC", "")
 
 @Client.on_message(filters.private & filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
-    if not await CustomFilters.authorized(None, update):
-        return await update.reply_text(text=Translation.USTART_TEXT.format(update.from_user.mention), disable_web_page_preview=True, reply_markup=Translation.USTART_BUTTONS)
+    try:
+        # Retrieve user data
+        bot_data = find_one(int(botid))
+        prrename = bot_data['total_rename']
+        prsize = bot_data['total_size']
+        user_deta = find_one(user_id)
+        
+        used_date = user_deta["date"]
+        buy_date = user_deta["prexdate"]
+        daily = user_deta["daily"]
+        user_type = user_deta["usertype"]
+    except:
+        await update.reply_text("Use About cmd first /about")
+        return
     
-    if Config.LOG_CHANNEL:
-        try:
-            log_message = await update.forward(Config.LOG_CHANNEL)
-            log_info = "Message Sender Information\n"
-            log_info += "\nFirst Name: " + update.from_user.first_name
-            log_info += "\nUser ID: " + str(update.from_user.id)
-            log_info += "\nUsername: @" + update.from_user.username if update.from_user.username else ""
-            log_info += "\nUser Link: " + update.from_user.mention
-            await log_message.reply_text(
-                text=log_info,
-                disable_web_page_preview=True,
-                quote=True
-            )
-        except Exception as error:
-            print(error)
+    c_time = time.time()
+    if user_type == "Free":
+        LIMIT = 600
+    else:
+        LIMIT = 50
+    
+    then = used_date + LIMIT
+    left = round(then - c_time)
+    conversion = datetime.timedelta(seconds=left)
+    ltime = str(conversion)
+    
+    if left > 0:
+        await update.reply_text(f"Sorry Dude, flood control is active, please wait for {ltime}.", reply_to_message_id=update.message.message_id)
+        return
+    
+    if buy_date == None:
+        await message.reply_text(
+            f"You can't upload more than {humanbytes(limit)}.\n\n"
+            f"Used daily limit: {humanbytes(used)}",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Upgrade 💰💳", callback_data="upgrade")]])
+        )
+        return
+    
+    pre_check = check_expi(buy_date)
+    if pre_check == False:
+        uploadlimit(message.from_user.id, 1288490188)
+        usertype(message.from_user.id, "Free")
+        await message.reply_text(f'Your plan expired on {buy_date}', quote=True)
+        return
+
+    if remain < int(file.file_size):
+        # User has exceeded daily limit
+        await message.reply_text(
+            f"100% of daily {humanbytes(limit)} data quota exhausted.\n\n"
+            f"File size detected: {humanbytes(file.file_size)}\n"
+            f"Used Daily Limit: {humanbytes(used)}\n\n"
+            f"You have only {humanbytes(remain)} left on your Account.\n"
+            f"If you want to rename large files, please upgrade your plan.",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Upgrade 💰💳", callback_data="upgrade")]])
+        )
+        return
+    else:
+
+       if Config.LOG_CHANNEL:
+           try:
+              log_message = await update.forward(Config.LOG_CHANNEL)
+              log_info = "Message Sender Information\n"
+              log_info += "\nFirst Name: " + update.from_user.first_name
+              log_info += "\nUser ID: " + str(update.from_user.id)
+              log_info += "\nUsername: @" + update.from_user.username if update.from_user.username else ""
+              log_info += "\nUser Link: " + update.from_user.mention
+              await log_message.reply_text(
+                  text=log_info,
+                  disable_web_page_preview=True,
+                  quote=True
+              )
+          except Exception as error:
+              print(error)
             
     if not update.from_user:
         return await update.reply_text("I don't know about you sar :(")
@@ -156,7 +211,7 @@ async def echo(bot, update):
             chat_id=update.chat.id,
             text=f'ᴘʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ ʟɪɴᴋ ⌛',
             disable_web_page_preview=True,
-            reply_to_message_id=update.id,
+            reply_to_message_id=update.message.message_id,
             parse_mode=enums.ParseMode.HTML
           )
     process = await asyncio.create_subprocess_exec(
@@ -180,7 +235,7 @@ async def echo(bot, update):
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.NO_VOID_FORMAT_FOUND.format(str(error_message)),
-            reply_to_message_id=update.id,
+            reply_to_message_id=update.message.message_id,
             parse_mode=enums.ParseMode.HTML,
             disable_web_page_preview=True
         )
@@ -202,64 +257,62 @@ async def echo(bot, update):
         if "duration" in response_json:
             duration = response_json["duration"]
         if "formats" in response_json:
-            for formats in response_json["formats"]:
-                format_id = formats.get("format_id")
-                format_string = formats.get("format_note")
-                if format_string is None:
-                    format_string = formats.get("format")
-                format_ext = formats.get("ext")
-                approx_file_size = ""
-                if "filesize" in formats:
-                    approx_file_size = humanbytes(formats["filesize"])
-                cb_string_video = "{}|{}|{}|{}".format(
-                    "video", format_id, format_ext, randem)
-                cb_string_file = "{}|{}|{}|{}".format(
-                    "file", format_id, format_ext, randem)
-                if format_string is not None and not "audio only" in format_string:
-                    ikeyboard = [
-                        InlineKeyboardButton(
-                            "🎬 " + format_string + " " + format_ext + " " + approx_file_size + " ",
-                            callback_data=(cb_string_video).encode("UTF-8")
-                        )
-                    ]
-                    """if duration is not None:
-                        cb_string_video_message = "{}|{}|{}|{}|{}".format(
-                            "vm", format_id, format_ext, ran, randem)
-                        ikeyboard.append(
+            # Check if the user has reached the daily limit before showing quality options
+            if left > 0:
+                for formats in response_json["formats"]:
+                    format_id = formats.get("format_id")
+                    format_string = formats.get("format_note")
+                    if format_string is None:
+                        format_string = formats.get("format")
+                    format_ext = formats.get("ext")
+                    approx_file_size = ""
+                    if "filesize" in formats:
+                        approx_file_size = humanbytes(formats["filesize"])
+                    cb_string_video = "{}|{}|{}|{}".format(
+                        "video", format_id, format_ext, randem)
+                    cb_string_file = "{}|{}|{}|{}".format(
+                        "file", format_id, format_ext, randem)
+                    if format_string is not None and not "audio only" in format_string:
+                        ikeyboard = [
                             InlineKeyboardButton(
-                                "VM",
-                                callback_data=(
-                                    cb_string_video_message).encode("UTF-8")
+                                "🎬 " + format_string + " " + format_ext + " " + approx_file_size + " ",
+                                callback_data=(cb_string_video).encode("UTF-8")
                             )
-                        )"""
-                else:
-                    # special weird case :\
-                    ikeyboard = [
+                        ]
+                    else:
+                        # special weird case :\
+                        ikeyboard = [
+                            InlineKeyboardButton(
+                                "🎬 [" +
+                                "] ( " +
+                                approx_file_size + " )",
+                                callback_data=(cb_string_video).encode("UTF-8")
+                            )
+                        ]
+                    inline_keyboard.append(ikeyboard)
+                if duration is not None:
+                    cb_string_64 = "{}|{}|{}|{}".format("audio", "64k", "mp3", randem)
+                    cb_string_128 = "{}|{}|{}|{}".format("audio", "128k", "mp3", randem)
+                    cb_string = "{}|{}|{}|{}".format("audio", "320k", "mp3", randem)
+                    inline_keyboard.append([
                         InlineKeyboardButton(
-                            "🎬 [" +
-                            "] ( " +
-                            approx_file_size + " )",
-                            callback_data=(cb_string_video).encode("UTF-8")
-                        )
-                    ]
-                inline_keyboard.append(ikeyboard)
-            if duration is not None:
-                cb_string_64 = "{}|{}|{}|{}".format("audio", "64k", "mp3", randem)
-                cb_string_128 = "{}|{}|{}|{}".format("audio", "128k", "mp3", randem)
-                cb_string = "{}|{}|{}|{}".format("audio", "320k", "mp3", randem)
-                inline_keyboard.append([
-                    InlineKeyboardButton(
-                        "🎵 ᴍᴘ𝟹 " + "(" + "64 ᴋʙᴘs" + ")", callback_data=cb_string_64.encode("UTF-8")),
-                    InlineKeyboardButton(
-                        "🎵 ᴍᴘ𝟹 " + "(" + "128 ᴋʙᴘs" + ")", callback_data=cb_string_128.encode("UTF-8"))
-                ])
-                inline_keyboard.append([
-                    InlineKeyboardButton(
-                        "🎵 ᴍᴘ𝟹 " + "(" + "320 ᴋʙᴘs" + ")", callback_data=cb_string.encode("UTF-8"))
-                ])
+                            "🎵 ᴍᴘ𝟹 " + "(" + "64 ᴋʙᴘs" + ")", callback_data=cb_string_64.encode("UTF-8")),
+                        InlineKeyboardButton(
+                            "🎵 ᴍᴘ𝟹 " + "(" + "128 ᴋʙᴘs" + ")", callback_data=cb_string_128.encode("UTF-8"))
+                    ])
+                    inline_keyboard.append([
+                        InlineKeyboardButton(
+                            "🎵 ᴍᴘ𝟹 " + "(" + "320 ᴋʙᴘs" + ")", callback_data=cb_string.encode("UTF-8"))
+                    ])
+                    inline_keyboard.append([                 
+                        InlineKeyboardButton(
+                            "⛔️ ᴄʟᴏsᴇ", callback_data='close')               
+                    ])
+            else:
+                # User has reached the daily limit, prompt to upgrade
                 inline_keyboard.append([                 
                     InlineKeyboardButton(
-                        "⛔️ ᴄʟᴏsᴇ", callback_data='close')               
+                        "You have reached your daily download limit. Upgrade to get more downloads.", callback_data='upgrade')               
                 ])
         else:
             format_id = response_json["format_id"]
@@ -291,7 +344,7 @@ async def echo(bot, update):
             text=Translation.FORMAT_SELECTION.format(Thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML,
-            reply_to_message_id=update.id
+            reply_to_message_id=update.message.message_id
         )
     else:
         # ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL
@@ -313,73 +366,5 @@ async def echo(bot, update):
             text=Translation.FORMAT_SELECTION,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML,
-            reply_to_message_id=update.id
+            reply_to_message_id=update.message.message_id
         )
-
-    c_time = time.time()
-
-    if user_type == "Free":
-        LIMIT = 600
-    else:
-        LIMIT = 50
-    then = used_date + LIMIT
-    left = round(then - c_time)
-    conversion = datetime.timedelta(seconds=left)
-    ltime = str(conversion)
-    if left > 0:
-        await message.reply_text(f"```Sorry Dude I am not only for YOU \n Flood control is active so please wait for {ltime}```", reply_to_message_id=message.id)
-    else:
-        # Forward a single message
-        media = await client.get_messages(message.chat.id, message.id)
-        file = media.document or media.video or media.audio
-        dcid = FileId.decode(file.file_id).dc_id
-        filename = file.file_name
-        value = 2147483648
-        used_ = find_one(message.from_user.id)
-        used = used_["used_limit"]
-        limit = used_["uploadlimit"]
-        expi = daily - int(time.mktime(time.strptime(str(date_.today()), '%Y-%m-%d')))
-        if expi != 0:
-            today = date_.today()
-            pattern = '%Y-%m-%d'
-            epcho = int(time.mktime(time.strptime(str(today), pattern)))
-            daily_(message.from_user.id, epcho)
-            used_limit(message.from_user.id, 0)
-        remain = limit - used
-        if remain < int(file.file_size):
-            await message.reply_text(f"100% of daily {humanbytes(limit)} data quota exhausted.\n\n  File size detected {humanbytes(file.file_size)}\n  Used Daily Limit {humanbytes(used)}\n\nYou have only **{humanbytes(remain)}** left on your Account.\nIf U Want to Rename Large File Upgrade Your Plan ", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Upgrade 💰💳", callback_data="upgrade")]]))
-            return
-        if value < file.file_size:
-            
-            if STRING:
-                if buy_date == None:
-                    await message.reply_text(f" You Can't Upload More Then {humanbytes(limit)} Used Daily Limit {humanbytes(used)} ", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Upgrade 💰💳", callback_data="upgrade")]]))
-                    return
-                pre_check = check_expi(buy_date)
-                if pre_check == True:
-                    await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {humanize.naturalsize(file.file_size)}\n**Dc ID** :- {dcid}""", reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📝 Rename", callback_data="rename"), InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]]))
-                    total_rename(int(botid), prrename)
-                    total_size(int(botid), prsize, file.file_size)
-                else:
-                    uploadlimit(message.from_user.id, 1288490188)
-                    usertype(message.from_user.id, "Free")
-
-                    await message.reply_text(f'Your Plan Expired On {buy_date}', quote=True)
-                    return
-            else:
-                await message.reply_text("Can't upload files bigger than 2GB ")
-                return
-        else:
-            if buy_date:
-                pre_check = check_expi(buy_date)
-                if pre_check == False:
-                    uploadlimit(message.from_user.id, 1288490188)
-                    usertype(message.from_user.id, "Free")
-
-            filesize = humanize.naturalsize(file.file_size)
-            fileid = file.file_id
-            total_rename(int(botid), prrename)
-            total_size(int(botid), prsize, file.file_size)
-            await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}\n**Dc ID** :- {dcid}""", reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("📝 Rename", callback_data="rename"),
-                  InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]]))
