41aa301760e2e4b73b9e6ed7cbbc06b627c2cd53
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-04-09 11:19:12 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -44,12 +44,12 @@ from plugins.database.database import daily as daily_
 from helper.date import check_expi
 import os
 
-CHANNEL = os.environ.get('CHANNEL', "")
+CHANNEL = os.environ.get('CHANNEL', "-1001580310925")
 STRING = os.environ.get("STRING", "")
-ADMIN = int(os.environ.get("ADMIN", 1484670284))
-bot_username = os.environ.get("BOT_USERNAME","GangsterBaby_renamer_BOT")
+ADMIN = int(os.environ.get("ADMIN", 6697298553))
+bot_username = os.environ.get("BOT_USERNAME","@TomenxLinkBot")
 log_channel = int(os.environ.get("LOG_CHANNEL", ""))
-token = os.environ.get('TOKEN', '')
+token = os.environ.get('TOKEN', '6416546477:AAFHEyibKRN7Jp8RxF73o-fRhrOmTqvgoSM')
 botid = token.split(':')[0]
 FLOOD = 500
 LAZY_PIC = os.environ.get("LAZY_PIC", "")
